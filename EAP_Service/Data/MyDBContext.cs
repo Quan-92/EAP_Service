2701a8caa94d3ab7294937e6ef1d74f2ddc5456f
TranQuan <9602tranquan@gmail.com>
2022-02-24 12:07:28 +0700

    Tranquan

---
+++ EAP_Service/Data/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using EAP_Service.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EAP_Service.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext():base ("name=EAPService")
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ EAP_Service/Entity/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace EAP_Service.Entity
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        [Key]
+        public int EmployeeID { get; set; }
+        [DataMember]
+        public string EmployeeName { get; set; }
+        [DataMember]
+        public double Salary { get; set; }
+        [DataMember]
+        public string Deparment { get; set; }
+
+
+    }
+}
